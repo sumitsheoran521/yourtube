b28c83b98b7fb5df94656cd0f1a2c062bdc8e84c
sumitsheoran521 <sumitsheoran521@gmail.com>
2025-03-12 00:18:59 +0530

    added structure

---
+++ src/Head.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <div>
+      <div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMLBqW4YiySKjx5nxeBXO33Kt9Ej9zLczyiuMDMhloK-zKIIhdGfas9oj7jmV0wXGBsvk&usqp=CAUhttps://img.freepik.com/premium-vector/hamburger-menu-bar-flat-icon-apps-vector_1254296-1292.jpg"
+          alt="menu"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/2560px-Logo_of_YouTube_%282015-2017%29.svg.png"
+          alt="logo"
+          className="w-36"
+        />
+      </div>
+      <div>
+        <input type="text"/>
+        <button>Search</button>
+      </div>
+      <div>
+        <img src="https://cdn-icons-png.flaticon.com/512/1144/1144760.png" alt="user" />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
